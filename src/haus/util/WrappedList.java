35b4816beee6814144a026968ab89931fd78d8ce
Matthew Hausknecht <matthew_hausknecht@yahoo.com>
2009-07-15 20:18:45 -0500

    Modified the Wrapped List class to remove some bugs.

--- src/haus/util/WrappedList.java
+++ src/haus/util/WrappedList.java
@@ -2,6 +2,7 @@ package haus.util;
 
 import java.io.Serializable;
 import java.util.AbstractList;
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
@@ -15,7 +16,7 @@ public class WrappedList<E> extends AbstractList<E>
 	/**
 	 * The array holding the actual elements
 	 */
-	private transient E[] elementData;
+	private E[] elementData;
 	
 	/**
 	 * The number of elements in the list
@@ -115,6 +116,19 @@ public class WrappedList<E> extends AbstractList<E>
 		size--;
 		return out;
 	}
+	
+	public E removeLast () {
+		if (isEmpty())
+			return null;
+		if (end == 0)
+			end = elementData.length-1;
+		else
+			end = (end - 1) % elementData.length;
+		E out = (E) elementData[end];
+		elementData[end] = null;
+		size--;
+		return out;
+	}
 
 	/**
 	 * Clears our wrapped list
@@ -124,6 +138,7 @@ public class WrappedList<E> extends AbstractList<E>
 			elementData[(start + i) % elementData.length] = null;
 		start = 0;
 		end = 0;
+		size = 0;
 	}
 
 	/**
@@ -156,16 +171,136 @@ public class WrappedList<E> extends AbstractList<E>
 		return sb.toString();
 	}
 	
+	/**
+	 * Linear time operation to find the specified element
+	 */
+	public int indexOf (Object elem) {
+		if (elem == null) {
+			for (int i = 0; i < size; i++)
+				if (elementData[(start + i) % elementData.length] == null)
+					return i;
+		} else {
+			for (int i = 0; i < size; i++)
+				if (elem.equals(elementData[(start + i) % elementData.length]))
+					return i;
+		}
+		return -1;
+	}
+	
+	/**
+	 * Linear time operation to perform the element shifting
+	 */
+	public E remove (int index) {
+		RangeCheck(index);
+		E out = elementData[(start + index) % elementData.length];
+		
+		for (int i = index; i < size-1; i++)
+			elementData[(start + i) % elementData.length] = elementData[(start + i + 1) % elementData.length];
+		removeLast();
+		
+		return out;
+	}
+	
+	/**
+	 * Linear time operation
+	 */
+	public boolean remove (Object elem) {
+		int index = indexOf(elem);
+		if (index < 0)
+			return false;
+		
+		if (remove(index) == null)
+			return false;
+		return true;
+	}
+	
 	private void RangeCheck (int index) {
 		if (index < 0 || index >= size)
 			throw new IndexOutOfBoundsException(
 					"Index: "+index+", Size: "+size);
 	}
+	
+	private boolean isEqual (List<Integer> other) {
+		if (size != other.size())
+			return false;
+		for (int i = 0; i < size; i++)
+			if (!get(i).equals(other.get(i)))
+				return false;
+		return true;
+	}
+	
+	public static void main (String[] args) {
+		WrappedList<Integer> w = new WrappedList<Integer>();
+		ArrayList<Integer> a = new ArrayList<Integer>();
+		
+		// Test ADD
+		for (int i = 0; i < 500; i++) {
+			int ran = (int) (Math.random() * 1000);
+			w.add(ran);
+			a.add(ran);
+			
+			if (!w.isEqual(a))
+				System.out.println("Failure: ADD");
+		}
+		
+		// Test SET
+		for (int i = 0; i < 1000; i++) {
+			int index = (int) (Math.random() * w.size);
+			int val = (int) (Math.random() * 1000);
+			
+			w.set(index, val);
+			a.set(index, val);
+			
+			if (!w.isEqual(a))
+				System.out.println("Failure: SET");
+		}
+		
+
+		// Test remove(Index)
+		for (int i = 0; i < 500; i++) {
+			int index = (int) (Math.random() * w.size);
+			
+			if (!w.remove(index).equals(a.remove(index)))
+				System.out.println("Failure: REMOVE(index)");
+			
+			if (!w.isEqual(a))
+				System.out.println("Failure: REMOVE(index)");
+		}
+		
+		// Test ADD
+		for (int i = 0; i < 500; i++) {
+			int ran = (int) (Math.random() * 1000);
+			w.add(ran);
+			a.add(ran);
+			
+			if (!w.isEqual(a))
+				System.out.println("Failure: ADD");
+		}
+		
+		// Test INDEXOF
+		for (int i = 0; i < 1000; i++) {
+			Integer val = (int) (Math.random() * 10000);
+			
+			if (w.indexOf(val) != a.indexOf(val))
+				System.out.println("Failure: indexOf");
+		}
+		
+		// Test Remove(object)
+		for (int i = 0; i < 500; i++) {
+			Integer val = (int) (Math.random() * 1000);
+			
+			w.remove(val);
+			a.remove(val);
+			
+			if (!w.isEqual(a))
+				System.out.println("Failure: REMOVE(obj)");
+		}
+		
+		System.out.println("Done");
+	}
 
 	public int lastIndexOf (Object arg0) {throw new UnsupportedOperationException();}
 	public boolean contains(Object elem) {throw new UnsupportedOperationException();}
-	public int indexOf(Object elem) {throw new UnsupportedOperationException();}
-	public E remove (int arg0) {throw new UnsupportedOperationException();}
 	public boolean containsAll(Collection<?> arg0) {throw new UnsupportedOperationException();}
 	public List<E> subList(int fromIndex, int toIndex) {throw new UnsupportedOperationException();}
 	public Object[] toArray() {throw new UnsupportedOperationException();}
@@ -175,8 +310,7 @@ public class WrappedList<E> extends AbstractList<E>
 	public boolean addAll(int arg0, Collection<? extends E> arg1) {throw new UnsupportedOperationException();}
 	public Iterator<E> iterator() {	throw new UnsupportedOperationException();}
 	public ListIterator<E> listIterator() {throw new UnsupportedOperationException();}
-	public ListIterator<E> listIterator(int arg0) {throw new UnsupportedOperationException();}
-	public boolean remove (Object arg0) {throw new UnsupportedOperationException();}	
+	public ListIterator<E> listIterator(int arg0) {throw new UnsupportedOperationException();}	
 	public boolean removeAll(Collection<?> arg0) {throw new UnsupportedOperationException();}
 	public boolean retainAll(Collection<?> arg0) {throw new UnsupportedOperationException();}
 }
